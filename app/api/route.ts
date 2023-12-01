a488d6e0d91c79acb85fe576120242409b107ebf
snoopy1224 <misty12024@gmail.com>
2023-12-02 02:43:09 +0900

    fix:route

--- app/api/route.ts
+++ app/api/route.ts
@@ -6,5 +6,5 @@ export async function GET(request: NextRequest) {
     data: "오늘도 코딩을 해보자",
   };
 
-  return Response.json(respones, { status: 200 });
+  return NextResponse.json(respones, { status: 200 });
 }
