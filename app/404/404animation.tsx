d4af91470c97c657f8d6dfeca8c33982f3ceb358
snoopy1224 <misty12024@gmail.com>
2024-01-28 22:45:50 +0900

    fix:lottie수정

--- app/404/404animation.tsx
+++ app/404/404animation.tsx
@@ -5,6 +5,5 @@ import Lottie from "react-lottie-player";
 import lottieJson from "@/public/404.json";
 
 export default function ErrorAnimation() {
-  // 미작동
   return <Lottie loop animationData={lottieJson} play />;
 }
