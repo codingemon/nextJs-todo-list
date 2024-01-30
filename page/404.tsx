9f08a105f1308a7370a8e824e3b5dfa28d73aafb
snoopy1224 <misty12024@gmail.com>
2024-01-30 20:30:50 +0900

    fix:404error

--- page/404.tsx
+++ page/404.tsx
@@ -1,6 +1,7 @@
 import ErrorAnimation from "@/app/404/404animation";
 
 export default function Custom404() {
+  // 에러 애니메이션 가져오기
   <>
     <h1 className="text-center title-font p-5 sm:text-4xl text-3xl mb-4 font-medium text-gray-900"></h1>
     <ErrorAnimation />
