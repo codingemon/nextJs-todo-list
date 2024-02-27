7db51122dfce4cc541a96c3d800db48a58996675
snoopy1224 <misty12024@gmail.com>
2024-02-27 21:37:21 +0900

    update

--- app/error.tsx
+++ app/error.tsx
@@ -10,21 +10,13 @@ export default function Error({
   reset: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
 
   return (
     <div>
       <h2>페이지에 문제가 생겼어요!!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        다시 시도해볼까요?
-      </button>
+      <button onClick={() => reset()}>다시 시도해볼까요?</button>
     </div>
   );
 }
